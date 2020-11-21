d2493b76f85d98f49e7f9a6fcb214b0a19d19cbf
d-otis <dan@dan-foley.com>
2020-11-21 16:52:45 -0500

    send ID to Light component

--- src/containers/LightsContainer.js
+++ src/containers/LightsContainer.js
@@ -4,7 +4,7 @@ import Light from '../components/lights/Light'
 const LightsContainer = ({ lights }) => {
 
   return (
-    Object.keys(lights).map(id => <Light key={id} light={lights[id]} />)
+    Object.keys(lights).map(id => <Light key={id} id={id} light={lights[id]} />)
   )
 }
 
