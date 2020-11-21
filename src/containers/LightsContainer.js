d533cdc8d67d9efd16b6c1c18f995ae5f269e420
d-otis <dan@dan-foley.com>
2020-11-21 16:04:53 -0500

    pass in proper prop into Light component

--- src/containers/LightsContainer.js
+++ src/containers/LightsContainer.js
@@ -4,7 +4,7 @@ import Light from '../components/lights/Light'
 const LightsContainer = ({ lights }) => {
 
   return (
-    Object.keys(lights).map(light => <Light light={light} />)
+    Object.keys(lights).map(id => <Light light={lights[id]} />)
   )
 }
 
