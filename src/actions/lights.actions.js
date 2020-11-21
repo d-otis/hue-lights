5c9b32d94653b00a0086388372c03b7638adab94
d-otis <dan@dan-foley.com>
2020-11-21 15:49:49 -0500

    define fetchLights() dispatch action

---
+++ src/actions/lights.actions.js
@@ -0,0 +1,11 @@
+import { BASE_URL } from '../utils/baseURL.utils'
+import { USERNAME } from '../utils/username.utils'
+
+export function fetchLights() {
+  return dispatch => {
+    dispatch({type: 'LOADING_LIGHTS'})
+    fetch(`${BASE_URL}/${USERNAME}/lights`)
+      .then(res => res.json())
+      .then(json => console.log(json))
+  }
+}
