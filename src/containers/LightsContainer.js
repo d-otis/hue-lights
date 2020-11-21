6e463b8c04eb45f0b88a2d8481c8430a87869677
d-otis <dan@dan-foley.com>
2020-11-21 17:52:04 -0500

    mapDispatchToProps for toggleLight()

--- src/containers/LightsContainer.js
+++ src/containers/LightsContainer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Light from '../components/lights/Light'
+import { toggleLight } from '../actions/lights.actions'
+import { connect } from 'react-redux'
 
-const LightsContainer = ({ lights }) => {
+const LightsContainer = ({ lights, ...otherProps }) => {
 
   return (
-    Object.keys(lights).map(id => <Light key={id} id={id} light={lights[id]} />)
+    Object.keys(lights).map(id => <Light key={id} id={id} light={lights[id]} {...otherProps} />)
   )
 }
 
-export default LightsContainer
+export default connect(null, { toggleLight })(LightsContainer)
