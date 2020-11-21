ab6bdc11a8bb55c6fdd090476e1fed0771e3f3af
d-otis <dan@dan-foley.com>
2020-11-21 16:58:59 -0500

    build out handleClick

--- src/components/lights/Light.js
+++ src/components/lights/Light.js
@@ -4,7 +4,23 @@ import { USERNAME } from '../../utils/username.utils'
 
 const Light = ({ id, light }) => {
 
-  const { name } = light
+  const { name, state } = light
+
+  const [ isOn, setIsOn ] = useState(state.on)
+
+  const handleClick = () => {
+    setIsOn(!isOn)
+    fetch(`${BASE_URL}/${USERNAME}/lights/${id}/state`, {
+      method: "PUT",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({"on": isOn})
+    })
+      .then(res => res.json())
+      .then(json => console.log(json))
+  }
 
   return(
     <h1> {name} </h1>
