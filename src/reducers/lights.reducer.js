1da36270ea54a7f80196a665572e8fe1e319bb94
d-otis <dan@dan-foley.com>
2020-11-21 16:02:28 -0500

    define case for SET_LIGHTS

--- src/reducers/lights.reducer.js
+++ src/reducers/lights.reducer.js
@@ -1,10 +1,15 @@
-const INITIAL_STATE = []
+const INITIAL_STATE = {}
 
 const lightsReducer = ( state = INITIAL_STATE, action ) => {
   switch (action.type) {
-    case "SET_LIGHTS":
-      debugger
+    case 'LOADING_LIGHTS':
+      console.log('lights are loading')
       return state
+    case "SET_LIGHTS":
+      return {
+        ...state,
+        ...action.payload
+      }
     default: 
       return state
   }
