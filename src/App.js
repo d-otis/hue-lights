bcffa44b76834ef89445664f795bacb13e7125d9
d-otis <dan@dan-foley.com>
2020-11-21 14:58:30 -0500

    add componentDidMount() fetch req

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
 import Light from './components/lights/Light'
+import {BASE_URL} from './utils/baseURL.utils'
+import {USERNAME} from './utils/username.utils'
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch(`${BASE_URL}/${USERNAME}`)
+      .then(res => res.json())
+      .then(json => console.log(json))
+  }
+
   render() {
     return (
       <div>
