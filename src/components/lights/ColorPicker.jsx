2c96433c0e4d90d8d5874f79e4c8f0f1888ca3ed
d-otis <dan@dan-foley.com>
2020-11-22 15:58:24 -0500

    add ColorPicker component

---
+++ src/components/lights/ColorPicker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { HuePicker } from 'react-color'
+
+const ColorPicker = ({ hue, sat }) => {
+  debugger
+  return(
+    <HuePicker />
+  )
+}
+
+export default ColorPicker
