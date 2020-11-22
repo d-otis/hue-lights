99f482b38cf3175c2d7c68236d119c34260c0092
d-otis <dan@dan-foley.com>
2020-11-22 13:12:54 -0500

    remove brightness-related items

--- src/components/lights/Light.js
+++ src/components/lights/Light.js
@@ -1,25 +1,23 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import PowerSwitch from './PowerSwitch'
-import BrightnessSlider from './BrightnessSlider'
-import { updateBrightness } from '../../actions/lights.actions'
 
-const Light = ({ id, light, toggleLight, updateBrightness }) => {
+const Light = ({ id, light, toggleLight }) => {
 
-  const { name, state: { on: isOn, bri: brightness } } = light
+  const { name, state: { on: isOn } } = light
 
-  const handleClick = () => {
-    toggleLight(id, !isOn)
-  }
+  // const handleClick = () => {
+  //   toggleLight(id, !isOn)
+  // }
+  console.log(`${name} == ${isOn ? 'ON' : 'OFF' }`)
 
   return(
     <React.Fragment>
       <h1 style={{color: isOn ? 'green' : 'red' }} > {name} </h1>
-      <PowerSwitch isOn={isOn} handleClick={handleClick} />
-      <BrightnessSlider brightness={brightness} id={id} />
+      <PowerSwitch isOn={isOn} toggleLight={toggleLight} id={id} />
+      {/* <BrightnessSlider brightness={brightness} id={id} updateBrightness={updateBrightness} /> */}
       <hr/>
     </React.Fragment>
   )
 }
 
-export default connect(null, { updateBrightness })(Light)
+export default Light
