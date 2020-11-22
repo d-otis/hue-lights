8fe4b69d242f585bf6de2a94035062c7778b05e2
d-otis <dan@dan-foley.com>
2020-11-22 13:29:16 -0500

    remove StrictMode

--- src/index.js
+++ src/index.js
@@ -11,11 +11,9 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
