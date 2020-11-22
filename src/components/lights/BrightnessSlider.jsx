e2c4a8655ce3037350cb05cc71b08226901e76e1
d-otis <dan@dan-foley.com>
2020-11-22 14:59:56 -0500

    remove material-ui slider components

--- src/components/lights/BrightnessSlider.jsx
+++ src/components/lights/BrightnessSlider.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Slider from '@material-ui/core/Slider'
-import { withStyles } from '@material-ui/core/styles';
 
 const BrightnessSlider = ({ id, brightness, updateBrightness }) => {
 
@@ -9,39 +7,10 @@ const BrightnessSlider = ({ id, brightness, updateBrightness }) => {
     updateBrightness(id, newVal)
   }
 
-  const PrettoSlider = withStyles({
-    root: {
-      color: '#52af77',
-      height: 8,
-    },
-    thumb: {
-      height: 24,
-      width: 24,
-      backgroundColor: '#fff',
-      border: '2px solid currentColor',
-      marginTop: -8,
-      marginLeft: -12,
-      '&:focus, &:hover, &$active': {
-        boxShadow: 'inherit',
-      },
-    },
-    active: {},
-    valueLabel: {
-      left: 'calc(-50% + 4px)',
-    },
-    track: {
-      height: 8,
-      borderRadius: 4,
-    },
-    rail: {
-      height: 8,
-      borderRadius: 4,
-    },
-  })(Slider);
-
-
   return(
-    <PrettoSlider valueLabelDisplay="auto" aria-label="pretto slider" value={brightness / 100} onChange={handleChange} />
+    <form>
+      <input type="range" min={0} max={254} value={brightness} onChange={handleChange} />
+    </form>
   )
 }
 
