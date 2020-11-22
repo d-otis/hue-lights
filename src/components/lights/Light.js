4c671316a2ac1270b64c3ab1356ef9e312a47539
d-otis <dan@dan-foley.com>
2020-11-22 15:59:13 -0500

    import and mount ColorPicker

--- src/components/lights/Light.js
+++ src/components/lights/Light.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PowerSwitch from './PowerSwitch'
 import BrightnessSlider from './BrightnessSlider'
+import ColorPicker from './ColorPicker'
 
 const Light = ({ id, light, ...otherProps }) => {
 
@@ -10,6 +11,7 @@ const Light = ({ id, light, ...otherProps }) => {
     <React.Fragment>
       <h1 style={{color: isOn ? 'green' : 'red' }} > {name} [{id}]</h1>
       <PowerSwitch isOn={isOn} {...otherProps} id={id} />
+      <ColorPicker {...otherProps} hue={hue} sat={sat} />
       <BrightnessSlider brightness={brightness} id={id} {...otherProps} />
       <hr/>
     </React.Fragment>
