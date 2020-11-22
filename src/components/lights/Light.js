cd8810be29547e04cc7d19141c5f8e653e417c1e
d-otis <dan@dan-foley.com>
2020-11-21 22:51:42 -0500

    import and destructure updateBrightness() into BrightnessSlider

--- src/components/lights/Light.js
+++ src/components/lights/Light.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux'
 import PowerSwitch from './PowerSwitch'
 import BrightnessSlider from './BrightnessSlider'
+import { updateBrightness } from '../../actions/lights.actions'
 
-const Light = ({ id, light, toggleLight }) => {
+const Light = ({ id, light, toggleLight, updateBrightness }) => {
 
   const { name, state: { on: isOn } } = light
 
@@ -15,10 +16,10 @@ const Light = ({ id, light, toggleLight }) => {
     <React.Fragment>
       <h1 style={{color: isOn ? 'green' : 'red' }} > {name} </h1>
       <PowerSwitch isOn={isOn} handleClick={handleClick} />
-      <BrightnessSlider />
+      <BrightnessSlider brightness={brightness} id={id} />
       <hr/>
     </React.Fragment>
   )
 }
 
-export default Light
+export default connect(null, { updateBrightness })(Light)
