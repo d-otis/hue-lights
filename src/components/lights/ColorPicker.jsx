8e8b77c0c1dd568167fc0253a611a9fac7ff6290
d-otis <dan@dan-foley.com>
2020-11-23 11:17:45 -0500

    import and use scale() in ColorPicker component

--- src/components/lights/ColorPicker.jsx
+++ src/components/lights/ColorPicker.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import { HuePicker } from 'react-color'
+import scale from '../../utils/scale.utils'
+
+const ColorPicker = ({ id, hue, sat, updateLight }) => {
+  const handleChange = color => {
+    const scaledHue = Math.round(scale(color.hsl.h))
+    console.log(scaledHue)
+    updateLight(id, "hue", scaledHue)
+  }
 
 const ColorPicker = ({ hue, sat }) => {
   debugger
