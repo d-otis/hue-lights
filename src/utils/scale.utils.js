7f65a5dccbe040269bb7e7685b64e15760b5c711
d-otis <dan@dan-foley.com>
2020-12-06 17:15:51 -0500

    refactor scale() to be more dynamic

--- src/utils/scale.utils.js
+++ src/utils/scale.utils.js
@@ -1,9 +1,18 @@
-const scale = (input, inputRange = [0, 360], outputRange = [0, 65535]) => {
-  const [outputMin, outputMax] = outputRange;
-  const [inputMin, inputMax] = inputRange;
+const scale = (input, config) => {
+  
+  if (config !== 'toBulb' && config !== 'toPicker') { 
+    console.error("scale() config string should be either 'toBulb' or 'toPicker'")
+    return
+   } 
+
+  const pickerRange = [0, 360]
+  const bulbRange = [0, 65535]
+
+  const [outputMin, outputMax] = config === 'toBulb' ? bulbRange : pickerRange
+  const [inputMin, inputMax] = config === 'toBulb' ? pickerRange : bulbRange
 
   const percent = (input - inputMin) / (inputMax - inputMin);
-  const output = percent * (outputMax - outputMin) + outputMin;
+  const output = Math.round(percent * (outputMax - outputMin) + outputMin);
 
   return output;
 };
