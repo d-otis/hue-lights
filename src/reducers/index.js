4669675ad3706a11d4ce340152431a657721ac61
d-otis <dan@dan-foley.com>
2020-12-06 18:49:17 -0500

    add groupsReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import lightsReducer from './lights.reducer'
+import groupsReducer from './groups.reducer'
 
 const rootReducer = combineReducers({
-  lights: lightsReducer
+  lights: lightsReducer,
+  groups: groupsReducer
 })
 
 export default rootReducer
