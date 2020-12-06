7563d22a2f7357fbafff10689b4189f6d05ec789
d-otis <dan@dan-foley.com>
2020-12-06 18:43:36 -0500

    add groups.reducer.js

---
+++ src/reducers/groups.reducer.js
@@ -0,0 +1,16 @@
+const INITIAL_STATE = {}
+
+const groupsReducer = ( state = INITIAL_STATE, action ) => {
+  switch (action.type) {
+    case 'LOADING_GROUPS':
+      console.log('loading groups')
+      return state
+    case 'SET_GROUPS':
+      debugger
+      return state
+    default:
+      return state
+  }
+}
+
+export default groupsReducer
