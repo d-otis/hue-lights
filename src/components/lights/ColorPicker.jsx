5051e4a3fc679b4f728950769a9e5664276f8d8c
d-otis <dan@dan-foley.com>
2020-12-06 17:17:14 -0500

    update scale() usage to reflect it's refactirong

--- src/components/lights/ColorPicker.jsx
+++ src/components/lights/ColorPicker.jsx
@@ -3,9 +3,9 @@ import { HuePicker } from 'react-color'
 import scale from '../../utils/scale.utils'
 
 const ColorPicker = ({ id, hue, sat, updateLight }) => {
-  const handleChange = color => {
-    const scaledHue = Math.round(scale(color.hsl.h))
-    console.log(scaledHue)
+
+  const handleChange = ({ hsl }) => {
+    const scaledHue = scale(hsl.h, "toBulb")
     updateLight(id, "hue", scaledHue)
   }
 
